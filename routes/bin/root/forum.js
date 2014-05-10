e376abe859649ba26bbb93f20a885f0230597caf
NiL <emlcjnil@gmail.com>
2014-05-10 11:43:35 +0800

    fixed AFTER event BUG

--- routes/bin/root/forum.js
+++ routes/bin/root/forum.js
@@ -1,18 +1,18 @@
 /*
 	GET /forum/:forumName
-*/
+	*/
 
-var ForumName = require('../../../models/forumName.js').forumName;
-var EventProxy = require('eventproxy');
+	var ForumName = require('../../../models/forumName.js').forumName;
+	var EventProxy = require('eventproxy');
 
-function get(req, res) {
-	ForumName.findOne({name: req.params.forumName}, function(err, forum){
-		var ep = new EventProxy();
-		if (err||!forum) {
-			console.log('There is no such forum');
-			req.flash('err', 'NO SUCH FORUM')
-			return res.redirect('back');
-		}
+	function get(req, res) {
+		ForumName.findOne({name: req.params.forumName}, function(err, forum){
+			var ep = new EventProxy();
+			if (err||!forum) {
+				console.log('There is no such forum');
+				req.flash('err', 'NO SUCH FORUM')
+				return res.redirect('back');
+			}
 		//console.log(forum);
 		forumInfor = {
 			forumId: forum.forumId,
@@ -20,7 +20,9 @@ function get(req, res) {
 		}
 		forum.populate('threadId', {}, function(err, newForum) {
 			if (err) {
+				console.log('THREADID POPULATE ERROR');
 				req.flash('error', err);
+				return res.redirect('back');
 			}
 			ep.emit('threadPopulated', newForum.threadId);
 		});
@@ -29,17 +31,23 @@ function get(req, res) {
 		ep.all('threadPopulated', function(threadId){
 			threadId.forEach(function(thread){
 				thread.populate('author', {}, function(err, newThread){
-					console.log(newThread);
+					if (err){
+						console.log('AUTHOR POPULATE ERROR');
+						return res.redirect('back');
+					}
 					ep.emit('authorPopulate', newThread);
 				});
 			});
 			if (threadId.length==0){
 				ep.emit('done');
 			}
+
+			//To ensure that AFTER time is right. So we must put it here
+			ep.after('authorPopulate', threadId.length, function(newThread){
+				ep.emit('done', newThread);
+			});
 		})
-		ep.after('authorPopulate', forum.threadId.length-1, function(newThread){
-			ep.emit('done', newThread);
-		});
+
 		ep.all('done', function(newThread){
 			if (!newThread) newThread = [];
 			res.render('./forum', {
